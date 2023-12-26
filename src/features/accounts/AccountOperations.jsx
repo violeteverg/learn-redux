6c8a36f9c48df1f9841edca716a161cbb3c19451
violeteverg <you@example.com>
2023-12-27 01:12:20 +0700

    upd: add another version

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -1,34 +1,36 @@
+// ==== ver learning ====
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   deposit,
-  withdraw,
-  payloan,
+  payLoan,
   requestLoan,
+  withdraw,
 } from "../../redux/slices/accountSlices";
 
-export default function AccountOperations() {
+function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
   const [withdrawalAmount, setWithdrawalAmount] = useState("");
   const [loanAmount, setLoanAmount] = useState("");
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
-  const dispatch = useDispatch();
-  const test = useSelector((state) => state.account.balance);
 
+  const dispatch = useDispatch();
   const {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
     isLoading,
-  } = useSelector((state) => state.account);
+  } = useSelector((store) => store.account);
+
+  console.log(balance);
 
   function handleDeposit() {
     if (!depositAmount) return;
+
     dispatch(deposit(depositAmount, currency));
-    setCurrency("USD");
     setDepositAmount("");
-    console.log(test);
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
@@ -45,7 +47,7 @@ export default function AccountOperations() {
   }
 
   function handlePayLoan() {
-    dispatch(payloan());
+    dispatch(payLoan());
   }
 
   return (
@@ -69,10 +71,9 @@ export default function AccountOperations() {
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
           </select>
-
           <button
             onClick={handleDeposit}
-            className="px-[6px] py-[2px] rounded-md bg-blue-500"
+            className={`px-[6px] py-[2px] rounded-md bg-blue-500`}
           >
             Deposit {depositAmount}
           </button>
@@ -134,3 +135,158 @@ export default function AccountOperations() {
     </div>
   );
 }
+
+export default AccountOperations;
+
+// ======= ver strugling ======
+
+// import { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   deposito,
+//   withdraw,
+//   payloan,
+//   requestLoan,
+//   convertingCurrency,
+//   updateBalanceAfterConversion,
+//   convertCurrency,
+// } from "../../redux/slices/accountSlices";
+
+// export default function AccountOperations() {
+//   const [depositAmount, setDepositAmount] = useState("");
+//   const [withdrawalAmount, setWithdrawalAmount] = useState("");
+//   const [loanAmount, setLoanAmount] = useState("");
+//   const [loanPurpose, setLoanPurpose] = useState("");
+//   const [currency, setCurrency] = useState("USD");
+//   const dispatch = useDispatch();
+
+//   const {
+//     loan: currentLoan,
+//     loanPurpose: currentLoanPurpose,
+//     balance,
+//     isLoading,
+//   } = useSelector((state) => state.account);
+
+//   async function handleDeposit() {
+//     if (!depositAmount) return;
+
+//     dispatch(convertingCurrency());
+
+//     try {
+//       let convertedAmount = depositAmount;
+//       if (currency !== "USD") {
+//         convertedAmount = await convertCurrency(depositAmount, currency);
+//       }
+
+//       await dispatch(deposito(convertedAmount, currency));
+//       dispatch(updateBalanceAfterConversion(convertedAmount));
+
+//       setCurrency("USD");
+//       setDepositAmount("");
+//     } catch (error) {
+//       console.error("Error depositing:", error);
+//     }
+//   }
+
+//   function handleWithdrawal() {
+//     if (!withdrawalAmount) return;
+//     dispatch(withdraw(withdrawalAmount));
+//     setWithdrawalAmount("");
+//   }
+
+//   function handleRequestLoan() {
+//     if (!loanAmount || !loanPurpose) return;
+//     dispatch(requestLoan(loanAmount, loanPurpose));
+//     setLoanAmount("");
+//     setLoanPurpose("");
+//   }
+
+//   function handlePayLoan() {
+//     dispatch(payloan());
+//   }
+
+// return (
+// <div className="mt-8">
+//   <h2 className="text-4xl">Your account operations</h2>
+//   <div className="bg-blue-200 p-8 rounded-lg ">
+//     <div className="px-[8px]">
+//       <label className="text-xl">Deposit</label>
+//       <input
+//         className="mx-2"
+//         type="number"
+//         value={depositAmount}
+//         onChange={(e) => setDepositAmount(+e.target.value)}
+//       />
+//       <select
+//         className="mr-2"
+//         value={currency}
+//         onChange={(e) => setCurrency(e.target.value)}
+//       >
+//         <option value="USD">US Dollar</option>
+//         <option value="EUR">Euro</option>
+//         <option value="GBP">British Pound</option>
+//       </select>
+//       <button
+//         onClick={handleDeposit}
+//         className={`px-[6px] py-[2px] rounded-md bg-blue-500`}
+//       >
+//         Deposit {depositAmount}
+//       </button>
+//     </div>
+
+//     <div className="px-[8px] my-2">
+//       <label className="text-xl">Withdraw</label>
+//       <input
+//         className="mx-2"
+//         type="number"
+//         value={withdrawalAmount}
+//         onChange={(e) => setWithdrawalAmount(+e.target.value)}
+//       />
+//       <button
+//         onClick={handleWithdrawal}
+//         className="px-[6px] py-[2px] rounded-md bg-blue-500"
+//       >
+//         Withdraw {withdrawalAmount}
+//       </button>
+//     </div>
+
+//     <div className="px-[8px] my-2">
+//       <label className="text-xl">Request loan</label>
+//       <input
+//         className="mx-2"
+//         type="number"
+//         value={loanAmount}
+//         onChange={(e) => setLoanAmount(+e.target.value)}
+//         placeholder="Loan amount"
+//       />
+//       <input
+//         className="mr-2"
+//         value={loanPurpose}
+//         onChange={(e) => setLoanPurpose(e.target.value)}
+//         placeholder="Loan purpose"
+//       />
+//       <button
+//         onClick={handleRequestLoan}
+//         className="px-[6px] py-[2px] rounded-md bg-blue-500"
+//       >
+//         Request loan
+//       </button>
+//     </div>
+
+//     {currentLoan > 0 && (
+//       <div className="my-2">
+//         <span className="text-xl mx-2">
+//           Pay back {currentLoan} {currentLoanPurpose}
+//         </span>
+//         <button
+//           onClick={handlePayLoan}
+//           className="px-[6px] py-[2px] rounded-md bg-blue-500"
+//         >
+//           Pay loan
+//         </button>
+//       </div>
+//     )}
+//   </div>
+// </div>
+// );
+// }
--- src/redux/slices/accountSlices.js
+++ src/redux/slices/accountSlices.js
@@ -1,18 +1,20 @@
+// ==== ver learning ====
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  loan: "",
-  balance: "",
+  balance: 0,
+  loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-const accountSlices = createSlice({
+const accountSlice = createSlice({
   name: "account",
   initialState,
   reducers: {
     deposit(state, action) {
-      (state.balance += action.payload), (state.isLoading = false);
+      state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -23,15 +25,16 @@ const accountSlices = createSlice({
           payload: { amount, purpose },
         };
       },
+
       reducer(state, action) {
         if (state.loan > 0) return;
 
         state.loan = action.payload.amount;
         state.loanPurpose = action.payload.purpose;
-        state.balance += action.payload.amount;
+        state.balance = state.balance + action.payload.amount;
       },
     },
-    payloan(state) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
@@ -41,6 +44,8 @@ const accountSlices = createSlice({
     },
   },
 });
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export function deposit(amount, currency) {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
@@ -58,5 +63,126 @@ export function deposit(amount, currency) {
   };
 }
 
-export const { withdraw, payloan, requestLoan } = accountSlices.actions;
-export default accountSlices.reducer;
+export default accountSlice.reducer;
+
+// export function deposit(amount, currency) {
+//   if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+//   return async function (dispatch, getState) {
+//     dispatch({ type: "account/convertingCurrency" });
+
+//     const res = await fetch(
+//       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+//     );
+//     const data = await res.json();
+//     const converted = data.rates.USD;
+
+//     dispatch({ type: "account/deposit", payload: converted });
+//   };
+// }
+
+// ===== ver strugling ======
+
+// import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   loan: 0,
+//   balance: 0,
+//   loanPurpose: "",
+//   isLoading: false,
+//   currency: "USD",
+// };
+
+// export const convertCurrency = async (amount, currency) => {
+//   try {
+//     const res = await fetch(
+//       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+//     );
+//     const data = await res.json();
+//     const converted = data.rates.USD;
+//     return converted;
+//   } catch (error) {
+//     console.error("Error converting currency:", error);
+//     throw error;
+//   }
+// };
+
+// export const deposito = createAsyncThunk(
+//   "account/deposit",
+//   async (amount, currency, { dispatch }) => {
+//     dispatch(convertingCurrency());
+
+//     try {
+//       let convertedAmount = amount;
+//       if (currency !== "USD") {
+//         convertedAmount = await convertCurrency(amount, currency);
+//       }
+
+//       return convertedAmount;
+//     } catch (error) {
+//       console.error("Error depositing:", error);
+//       throw error;
+//     }
+//   }
+// );
+
+// export const updateBalanceAfterConversion = createAction(
+//   "account/updateBalanceAfterConversion",
+//   (convertedAmount) => {
+//     return {
+//       payload: convertedAmount,
+//     };
+//   }
+// );
+
+// const accountSlice = createSlice({
+//   name: "account",
+//   initialState,
+//   reducers: {
+//     deposit(state, action) {
+//       state.balance += action.payload;
+//       state.isLoading = false;
+//     },
+//     withdraw(state, action) {
+//       state.balance -= action.payload;
+//     },
+//     requestLoan: {
+//       prepare(amount, purpose) {
+//         return {
+//           payload: { amount, purpose },
+//         };
+//       },
+//       reducer(state, action) {
+//         if (state.loan > 0) return;
+
+//         state.loan = action.payload.amount;
+//         state.loanPurpose = action.payload.purpose;
+//         state.balance += action.payload.amount;
+//       },
+//     },
+//     payloan(state) {
+//       state.balance -= state.loan;
+//       state.loan = 0;
+//       state.loanPurpose = "";
+//     },
+//     convertingCurrency(state) {
+//       state.isLoading = true;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder.addCase(deposito.fulfilled, (state, action) => {
+//       state.balance += action.payload;
+//       state.isLoading = false;
+//     });
+
+//     builder.addCase(updateBalanceAfterConversion, (state, action) => {
+//       state.balance += action.payload;
+//       state.isLoading = false;
+//     });
+//   },
+// });
+
+// export const { deposit, withdraw, payloan, requestLoan, convertingCurrency } =
+//   accountSlice.actions;
+
+// export default accountSlice.reducer;
